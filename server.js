dced118ea994b9260ba22d8b7db0e19a468dbddd
Karelyn Rojas <knrojas@ucdavis.edu>
2023-06-20 14:29:56 -0700

    working on routes

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 //1) Packages required by your application 
 const express = require('express');  // Import Express.js
-const path = require('path'); // Imports built-in Node.js package 'path' to resolve path files that are located on the server
+const path = require('path'); // Imports Node.js package 'path' to resolve path files 
 //const fs =require('fs');
+const app = express(); // Initiates Express.js
+//const =require('.Develop/db)
 const PORT = 3001; //Identifies which port Express.js server runs =process.env.PORT || 3001;
-const app = express(); // Starts Express.js
-//const =require('.Develop/?)
 
-//2) Middleware has access to req,res, and the next function. Middlware executes from top to bottom 
-app.use(express.json());//Middleware that parses application.json
-//Parsing: Parsing means analyzing and converting a program into an internal format that a runtime environment can actually run
-app.use(express.urlencoded({extended:true}));//and urlencoded data
-app.use(express.static('./develop/public'));//Static middleware pointing to the public folder w/in Develp(public has: assets w/css, js w/index.js,index.html and notes.html)
 
-//3) Express.js routes
-//app.get('/'(res,res) => res.send(''));
-app.get('/send', (req, res) =>
-    res.sendFile(path.join(__dirname,'./public/index.html'))
-);
-app.get('',(req, res) =>
-    res.sendFile(path.join(__dirname,'./public/notes.html'))
-);
+//2) Middleware that have access to req,res, and the next function(s). Middlware executes from top to bottom 
+app.use(express.json());//Middleware that parses application.json and urlencoded data
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('./Develop/public'));//Static middleware pointing to the public folder w/in Develop
+
+//3) Express.js routes || Get Requests || API Routes
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname,'./Develop/public/index.html'))
+);//Default route back to homepage
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname,'./Develop/public/notes.html'))
+);//Serving another file, specifically notes.html, from the public directory
+app.get('/index', (req, res) => 
+    res.sendFile(path.join(__dirname,'./Develop/public/index.html'))
+);//Serving another file,specifically index.html, from the public directory
 
 //4) Set up server to listen
 app.listen(PORT,()=>
